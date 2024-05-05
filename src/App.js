298f71ec1f2a4529f76f5dfc95c70672edf59032
mohamedsharrif <sharifmohamedmahamud@gmail.com>
2024-05-05 17:51:57 +0300

    Completed header layout and styling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./components/Header";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pajes/Home";
@@ -7,9 +7,11 @@ import Contact from "./pajes/Contact";
 import Cart from "./pajes/Cart";
 
 const App = () => {
+  const[cart,setCart] = useState(10)
   return (
-    <div>
-      <Header />
+    <div className="container">
+      <Header cart={cart}/>
+      
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,16 +2,17 @@ import React from 'react'
 import '../style.css'
 import { Link } from 'react-router-dom'
 
-const Header = () => {
+const Header = ({cart}) => {
   return (
     <div className='menu'>
-      <Link to='/'>CoverMeChic</Link>
-      <div>
+      <Link className='logo' to='/'>CoverMeChic</Link>
+      <div className='links'>
       <Link to='/'>Home</Link>
       <Link to='/about'>About</Link>
       <Link to='/contact'>Contact</Link>
       <Link to='/cart'>Cart</Link>
-      </div>
+       </div>
+      <Link to={'/cart'}><span className='cart'>{cart}</span></Link>
     </div>
   )
 }
