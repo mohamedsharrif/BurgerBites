2b66d9d5cff40857295a6a6c7971598742e519cb
mohamedsharrif <sharifmohamedmahamud@gmail.com>
2024-05-05 17:10:22 +0300

    Added React Router DOM links for navigation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,17 @@
 import React from 'react'
 import '../style.css'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
     <div className='menu'>
-      <a href="/">Logo</a>
-      <ul className='menu-item'>
-        <li>
-            <a href="/">Home</a>
-        </li>
-        <li>
-            <a href="/about">About</a>
-        </li>
-        <li>
-            <a href="/contact">Contact</a>
-        </li>
-        <li>
-            <a href="/cart">Cart</a>
-        </li>
-      </ul>
-      <a href="">5</a>
+      <Link to='/'>CoverMeChic</Link>
+      <div>
+      <Link to='/'>Home</Link>
+      <Link to='/about'>About</Link>
+      <Link to='/contact'>Contact</Link>
+      <Link to='/cart'>Cart</Link>
+      </div>
     </div>
   )
 }
